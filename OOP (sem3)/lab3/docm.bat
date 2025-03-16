620e2212f77f3ddc4a6ba553e24d1b88565a98bf
kirpaytik <114250746+kirpaytik@users.noreply.github.com>
2025-03-16 13:59:53 +0300

    Delete OOP (sem3)/lab3 directory
    Хочу переназвать папку

--- OOP (sem3)/lab3/docm.bat
+++
@@ -1,5 +0,0 @@
-cd build
-rm * -rf
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=D:/mingw32/bin/gcc.exe -DCMAKE_CXX_COMPILER=D:/mingw32/bin/g++.exe ..
-mingw32-make
-cd ..
--- OOP (sem3)/lab3/dogcc.bat
+++
@@ -1,3 +0,0 @@
-cd cpp
-g++ -o ../app.exe BuildAttack.cpp Castle.cpp Cell.cpp Controller.cpp DefBuild.cpp EffectAttack.cpp Enemy.cpp Field.cpp Game.cpp Lair.cpp Landscape.cpp MagicTower.cpp MagicTrap.cpp main.cpp parsing.cpp PriceTable.cpp Road.cpp Tower.cpp View.cpp -ID:/SFML-2.6.2-sources/include -LD:/SFML-2.6.2-sources/build/lib -lsfml-graphics -lsfml-window -lsfml-system -lopengl32 -lgdi32 -lwinmm -ldsound
-cd ..
