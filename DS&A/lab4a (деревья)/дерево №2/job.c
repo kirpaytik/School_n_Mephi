8f7d3d474ac4d2d4d88b77dcdfd4a06ec1b5ee91
kirpaytik <114250746+kirpaytik@users.noreply.github.com>
2025-03-16 12:40:30 +0300

    Add files via upload
    АиСД сем1 парт 1/2

---
+++ DS&A/lab4a (деревья)/дерево №2/job.c
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include "mylib.h"
+#include "tree.h"
+
+#define OK 0
+#define MEMORY 4
+
+
+short create_tree(Tree** dest)
+{
+	return t_create(dest);
+}
+
+
+void endofprog(Tree* tree)
+{
+	t_free(tree);
+	free(tree);
+}
+
+
+short start_program(Tree* tree)
+{
+	short checker = dialogue_menu(tree);
+	return checker;
+}
+
+
+short job_insert(Tree* tree, char* key, unsigned int info)
+{
+	KeySpace ks = (KeySpace){key, info};
+	return t_insert(tree, ks);
+}
+
+
+short job_delete(Tree* tree, char* key, short mode)
+{
+	short checker = t_delete(tree, key);
+	if (checker)
+		return checker;
+	if (mode == 0)
+		return OK;
+
+	while (!checker) {
+		checker = t_delete(tree, key);
+	}
+	if (checker == MEMORY)
+		return checker;
+	return OK;
+}
+
+
+void job_clear(Tree* tree)
+{
+	t_free(tree);
+}
+
+
+void job_traversal(Tree* tree, char* key, short mode)
+{
+	switch (mode) {
+	case -1:
+		t_preOrder_traversal(tree, key);
+		break;
+	case 0:
+		t_inOrder_traversal(tree, key);
+		break;
+	case 1:
+		t_postOrder_traversal(tree, key);
+		break;
+	}
+}
+
+
+short job_print(Tree* tree, FILE* file, short mode)
+{
+	switch (mode) {
+	case 0:
+		t_print_console_type1(tree);
+		return OK;
+	case 1:
+		return t_print_console_type2(tree);
+	case 2:
+		t_print_txtfile(tree, file);
+		return OK;
+	}
+}
+
+
+short job_find(Tree* tree, char* key, short mode,
+    char** foundkey, unsigned int* amount, unsigned int** infos)
+{
+	short checker;
+	Node* ptr;
+	if (mode == 0) {
+		checker = t_find(tree, key, &ptr);
+	}
+	else {
+		checker = t_special_search(tree, key, &ptr);
+	}
+	if (checker)
+		return checker;
+
+	*foundkey = ptr->key;
+	*amount = ptr->amount;
+	*infos = ptr->infos;
+	return OK;
+}
